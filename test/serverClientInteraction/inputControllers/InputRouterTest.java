b2f42680cb1c3cf30d638e87ee22b2c1aa4888ee
Kirill <shiruaev.k@gmail.com>
2018-01-25 00:08:19 +0300

    Add test for InputRouter

---
+++ test/serverClientInteraction/inputControllers/InputRouterTest.java
@@ -0,0 +1,20 @@
+package serverClientInteraction.inputControllers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputRouterTest {
+
+    @Test
+    void input() {
+
+        String deviceID = "kitchenSensor";
+        Integer externalSensorSignal = 1;
+
+        InputRouter<String, Integer> integerInputRouter = new InputRouter<>();
+        integerInputRouter.input(deviceID, externalSensorSignal);
+
+        assertEquals(deviceID, integerInputRouter.getDeviceID());
+        assertEquals(externalSensorSignal, integerInputRouter.getSensorSignal());
+    }
+}
