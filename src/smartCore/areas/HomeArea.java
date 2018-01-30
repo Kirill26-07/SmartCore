86ec146816e3d3a86ea039e30564b64cc8370410
Kirill <shiruaev.k@gmail.com>
2018-01-30 15:02:43 +0300

    Update Class HomeArea

--- src/smartCore/areas/HomeArea.java
+++ src/smartCore/areas/HomeArea.java
@@ -1,5 +1,6 @@
 package smartCore.areas;
 
+import smartCore.energyControllers.ClimateController;
 import smartCore.energyControllers.Lighting;
 import smartCore.securityControllers.AccessController;
 import smartCore.securityControllers.GasController;
@@ -9,9 +10,80 @@ public class HomeArea {
 
     private String name;
 
-    Lighting lightingConrtoler;
-    GasController gasController;
-    WaterController waterController;
-    AccessController accessController;
+    private Lighting lightingController;
+    private GasController gasController;
+    private WaterController waterController;
+    private AccessController accessController;
+    private ClimateController climateController;
 
+
+    HomeArea(String name, Lighting lightingController){
+
+        this(name, lightingController, null, null, null, null);
+
+    }
+
+    HomeArea(String name, Lighting lightingController, GasController gasController){
+
+        this(name, lightingController, gasController, null, null, null);
+
+    }
+
+    HomeArea(String name,
+             Lighting lightingController,
+             GasController gasController,
+             WaterController waterController){
+
+        this(name, lightingController, gasController, waterController, null, null);
+
+    }
+
+    HomeArea(String name,
+             Lighting lightingController,
+             GasController gasController,
+             WaterController waterController,
+             AccessController accessController){
+
+        this(name, lightingController, gasController, waterController, accessController, null);
+
+    }
+
+    HomeArea(String name,
+             Lighting lightingController,
+             GasController gasController,
+             WaterController waterController,
+             AccessController accessController,
+             ClimateController climateController){
+
+        this.name = name;
+        this.lightingController = lightingController;
+        this.gasController = gasController;
+        this.waterController = waterController;
+        this.accessController = accessController;
+        this.climateController = climateController;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Lighting getLightingController() {
+        return lightingController;
+    }
+
+    public GasController getGasController() {
+        return gasController;
+    }
+
+    public WaterController getWaterController() {
+        return waterController;
+    }
+
+    public AccessController getAccessController() {
+        return accessController;
+    }
+
+    public ClimateController getClimateController() {
+        return climateController;
+    }
 }
