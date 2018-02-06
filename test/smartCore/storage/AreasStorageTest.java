5093e093235ad8be545ebe54d5ad967539e70c09
Kirill <shiruaev.k@gmail.com>
2018-02-06 18:24:45 +0300

    Update tests

--- test/smartCore/storage/AreasStorageTest.java
+++ test/smartCore/storage/AreasStorageTest.java
@@ -1,5 +1,17 @@
+package smartCore.storage;
+
+import org.junit.jupiter.api.Test;
+import smartCore.areas.HomeArea;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AreasStorageTest {
 
+
+    @Test
+    void getHomeArea() {
+        HomeArea testHomeArea = new HomeArea("TestArea");
+        AreasStorage.addHomeArea(testHomeArea);
+        assertEquals(testHomeArea, AreasStorage.getHomeArea(0));
+    }
 }
--- test/smartCore/storage/UserStorageTest.java
+++ test/smartCore/storage/UserStorageTest.java
@@ -1,5 +1,35 @@
+package smartCore.storage;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import smartCore.autorithation.User;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserStorageTest {
 
+    private static String userName = "Kirill";
+    private static String userPassword = "123456";
+    private static final int idOfFirstUser = 0;
+
+    @BeforeAll
+    static void createTestUser(){
+        User testUser = new User(userName, userPassword);
+        UserStorage.saveNewUser(testUser);
+    }
+
+    @Test
+    void getUser() {
+
+        assertEquals(userName, UserStorage.getUser(0).getUserName());
+        assertEquals(userPassword, UserStorage.getUser(0).getUserPassword());
+
+    }
+
+//    @Test
+//    void getUserID() {
+//        createTestUser();
+//
+//
+//    }
 }
