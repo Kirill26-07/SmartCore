8c4f7f72e7879311f07c065c204ee4a18198c952
Kirill <shiruaev.k@gmail.com>
2018-03-09 23:39:35 +0300

    Remove files

--- SmartCore_Maven/src/main/java/kirill/smartCore/smartCore/controllers/serverClientInteraction/outputController/IOutputRouter.java
+++
@@ -1,8 +0,0 @@
-package kirill.smartCore.smartCore.controllers.serverClientInteraction.outputController;
-
-public interface IOutputRouter {
-
-    void output(String command);
-    void output(String controllerID, boolean controllerState);
-    void output(String controllerID, boolean controllerState, int value);
-}
